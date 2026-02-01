91e6c188d2b9b116ee054f13b8124f340a1a4eeb
mirhuseynma <mirhuseynma@code.edu.az>
2026-02-01 19:38:27 +0400

    ADDTasks

--- test/Program.cs
+++ test/Program.cs
@@ -10,3 +10,16 @@ int ba = 200;
 Console.WriteLine("iki ededinin cemi " + ab + ba);
 #endregion
 
+#region (Ədədin kvadratı Tapşırıq: Daxil edilən ədədin kvadratını hesablayın.)
+int q = 2;
+int squareNum = q * q;
+Console.WriteLine(q + " ededinin kvadrati " + squareNum);
+#endregion
+
+#region (Düzbucaqlının sahəsi və perimetri. Tapşırıq: Eni və uzunluğu verilən düzbucaqlının sahəsini və perimetrini tapın.)
+int sideOne = 6;
+int sideSecond = 10;
+int area = sideOne * sideSecond;
+int perimetr = 2 * (sideOne + sideSecond);
+Console.WriteLine("duzbucaqlinin sahesi " + area + ", perimetri ise " + perimetr);
+#endregion
