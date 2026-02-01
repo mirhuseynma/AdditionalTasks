624796b6ce59c8681bffb6d382f9e626dc4f25a3
mirhuseynma <mirhuseynma@code.edu.az>
2026-02-01 19:33:40 +0400

    TESTS

---
+++ test/Program.cs
@@ -0,0 +1,17 @@
+﻿#region ededin quvveti
+int num = 81;
+int quvvet = 1;
+int esas = 2;
+int numclone = num;
+while(quvvet < num)
+{
+    quvvet *= esas;  
+}
+ if (quvvet == num)
+    {
+        Console.WriteLine(num + " ededi " + esas + " ededinin quvvetidir");
+
+    }
+    else if(quvvet != num)
+        Console.WriteLine(num + " ededi " + esas + " ededinin quvveti deyil");
+#endregion
